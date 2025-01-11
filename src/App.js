f75ca68aa177d996cba02a219ba74e82fce96df6
Skaishgggg <swakaish444@gmail.com>
2025-01-11 13:12:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react'
 import './App.css';
-
+import { Route,Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Projects from './pages/Projects';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Footer from './pages/Footer';
 function App() {
   return (
-    <div className="App">
-     <h1> project fair</h1>
+    <div>
+      <Routes>
+        <Route  path='./' element={<Home />}/>
+        <Route  path='/login' element={<Login />}/>
+        <Route  path='/register' element={<Register />}/>
+        <Route  path='./dashboard' element={<Dashboard />}/>
+        <Route  path='./projects' element={<Projects />}/>
+      </Routes>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
