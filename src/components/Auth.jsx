30c5bfcdd83d144c5f6e1b7fd1c48bb83546f1cc
Skaishgggg <swakaish444@gmail.com>
2025-02-08 12:41:47 +0530

    c10

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -14,9 +14,9 @@ function Auth({register}) {
                         <img style={{width:'100%'}} src="https://img.freepik.com/free-vector/computer-login-concept-illustration_114360-7892.jpg?semt=ais_hybrid" alt="" />
                     </div>
                     <div className="col-lg-6">
-                       <div className='d-flex align-items-center flex-column'></div> 
+                       <div  className='d-flex align-items-center flex-column '>
                        <h1 style={{fontSize:'20px'}} className='fw-bolder d-flex align-items-center text-light' ><i className="fa-solid fa-list-check overflow-hidden"></i>Project Fair</h1>
-                        <h5 className='fw-bolder mt-4 pb-3 text-light'>
+                        <div className='fw-bolder mt-1 pb-2 text-light'>
                        {
                                  isRegisterForm ? 'sign up Your Account':'sign in Your Account'
                           }
@@ -24,21 +24,33 @@ function Auth({register}) {
 
                           {
                           isRegisterForm &&
-                          <Form.Group className='mb-3' controlId="formBasicName">
-                            <Form.Group type='text' placeholder='username'></Form.Group>
+                          <Form.Group className='mb-3 rounded-pill ' controlId="formBasicName">
+                                 <Form.Control type='text' placeholder='Username' />
                           </Form.Group>
+
 
                            }
-                           <Form.Group className='mb-3' controlId="formBasicEmail">
-                            <Form.Group type='text' placeholder='EmailId'></Form.Group>
-                          </Form.Group>
-                          <Form.Group className='mb-3' controlId="formBasicpswd">
-                            <Form.Group type='text' placeholder='Pasword'></Form.Group>
-                          </Form.Group>
+                                <Form.Group className='mb-3 rounded-pill' controlId="formBasicEmail">
+                                    <Form.Control type='email' placeholder='Email ID' />
+                                     </Form.Group>
 
+                          <Form.Group className='mb-3 rounded-pill' controlId="formBasicpswd">
+                            <Form.Control type='text' placeholder='Pasword'></Form.Control>
+                          </Form.Group>
+{
+              isRegisterForm ?
+              <div>
+                <button className='btn btn-primary  rounded-pill'>register</button>
+                <p>Already have Account? Click here to <Link to={'/login'}>Login</Link></p>
+              </div>:<div>
+              <button className='btn btn-primary rounded-pill'>Login</button>
+              <p>New User? Click here to <Link to={'/register'}>Register</Link></p>
+              </div>
+}
                           </Form>
-                        </h5>
                         </div>
+                        </div>
+                        </div> 
                     </div>
                 </div>
               </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,12 +12,12 @@ function Home() {
        <div className='Row align-items-center p-5 d-flex'>
 
           <Col sm={12} md={6}>
-            <h1 style={{fontSize:'80px'}} className='fw-bolder d-flex align-items-center ' ><i className="fa-solid fa-list-check overflow-hidden"></i>Project Fair</h1>
+            <h1 style={{fontSize:'50px'}} className='fw-bolder d-flex align-items-center ' ><i className="fa-solid fa-list-check overflow-hidden "></i>Project Fair</h1>
 
             <p>one stop destination for all software development projects.where user can add and manage their projects.As well as access all projects available in our website...what are you waiting forr!!!</p>
           <Link to={'/login'}  className='btn btn-warning'>Start to Explore <i className="fa-solid fa-arrow-right ms-2"></i>  </Link>
           </Col>
-          <Col><img  src="https://avatars.githubusercontent.com/u/87497641?v=4" alt="" /></Col>
+          <Col><img style={{width:'400px'}} src="https://avatars.githubusercontent.com/u/87497641?v=4" alt="" /></Col>
          </div> 
       </div>
       {/* all projects */}
