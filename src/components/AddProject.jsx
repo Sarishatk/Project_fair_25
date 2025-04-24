adad4f12c51c093422080e6ab00fe3906ceb0b40
Skaishgggg <sarishakrishna58@gmail.com>
2025-04-24 08:19:04 +0530

    f7

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { useState
-} from 'react';
+
+import { useState,useEffect} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function AddProject() {
+  
   const [show, setShow] = useState(false);
 
 const [projectDetails,setprojectDetails] = useState({
@@ -17,6 +18,7 @@ const [projectDetails,setprojectDetails] = useState({
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   console.log(projectDetails);
+
   
   return (
     <>
@@ -34,7 +36,11 @@ const [projectDetails,setprojectDetails] = useState({
         <Modal.Body>
 <div className="row">
   <div className="col"> 
-<img style={{height:'300px',width:'300px'}} className='image-fluid ' src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=" alt="profile picture" />
+   <label>
+      
+      <input type='file' style={{display:'none'}} onChange={e=>setprojectDetails({...projectDetails,projectImage:e.target.files[0]})} />
+  <img style={{height:'300px',width:'300px'}} className='image-fluid ' src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=" alt="profile picture" />
+   </label>
   </div>
   <div className="col "> 
 <div className=' mb-3 '><input type='text' className='form-control' placeholder='project title' value={projectDetails.title} onChange={e=>setprojectDetails({...projectDetails,title:e.target.value})}></input></div>
