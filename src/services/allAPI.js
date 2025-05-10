cff5e1e6c0b5093e74fe44f5bcec77fe18e4a643
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-10 14:15:09 +0530

    all projectapi

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -23,3 +23,6 @@ export const HomeProject = async () => {
   };
 
 //   all projects
+export const allProjectApi = async (reqHeader) => {
+    return await commonAPI("GET", `${BASE_URL}/project/all`,"",reqHeader);
+  };
