d47ab9a1666bc2258e8cca4f350204c3b9d81b61
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-17 12:44:15 +0530

    m10

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -3,55 +3,55 @@ import AddProject from './AddProject'
 import { userProjectAPI } from '../services/allAPI';
 function MyProject() {
 
-  const [userProjects,setuserProjects] = useState([])
+  const [userProjects, setuserProjects] = useState([])
 
-  const getUserProject = async ()=>{
+  const getUserProject = async () => {
     // before calling api token is created here
-    if(sessionStorage.getItem('token')){
-    const token =   sessionStorage.getItem('token')
-     const reqHeader = {
-          "content-type": "multipart/form-data",
-          "Authorization": `Bearer ${token}`,
-        };
+    if (sessionStorage.getItem('token')) {
+      const token = sessionStorage.getItem('token')
+      const reqHeader = {
+        "content-type": "multipart/form-data",
+        "Authorization": `Bearer ${token}`,
+      };
 
-        const result = await userProjectAPI(reqHeader)
-        if(result.status === 200){
-         setuserProjects(result.data);
-          
-        }else{
-          console.log(result);
-          console.log(result.response.data);
-          
-          
-        }
+      const result = await userProjectAPI(reqHeader)
+      if (result.status === 200) {
+        setuserProjects(result.data);
+
+      } else {
+        console.log(result);
+        console.log(result.response.data);
+
+
+      }
     }
   }
 
-  useEffect(()=>{
-getUserProject()
-  },[])
+  useEffect(() => {
+    getUserProject()
+  }, [])
 
   return (
-   <div className='card shadow mt-2 container-fluid'>
-    <div className='d-flex'>
+    <div className='card shadow mt-2 container-fluid'>
+      <div className='d-flex'>
         <h2>My projects</h2>
-        <div className='ms-auto'><AddProject/></div>
+        <div className='ms-auto'><AddProject /></div>
+      </div>
+      <div className='mt-4'>
+        {/* collection of user projects */}
+        {userProjects?.length > 0 ? userProjects.map(project => (<div className='border d-flex align-items-center rounded p-2'>
+          <h5>{project.title}</h5>
+          <div className='icon ms-auto'>
+            <button className='btn'><i class="fa-solid fa-pen-to-square"></i></button>
+            <button className='btn'><i class="fa-brands fa-github"></i></button>
+            <button className='btn'><i class="fa-solid fa-trash"></i></button>
+          </div>
         </div>
-        <div className='mt-4'>
-     {/* collection of user projects */}
-    {  userProjects?.length>0? userProjects.map(project=>(<div className='border d-flex align-items-center rounded p-2'>
-<h5>{project.title}</h5>
-<div className='icon ms-auto'>
-    <button  className='btn'><i class="fa-solid fa-pen-to-square"></i></button>
-    <button className='btn'><i class="fa-brands fa-github"></i></button>
-    <button className='btn'><i class="fa-solid fa-trash"></i></button>
-</div>
-     </div>
-     )):
-     <p className='text-danger fw-bolder fs-5'>No projects uploaded yet!!!</p>
-    }
-        </div>
-   </div>
+        )) :
+          <p className='text-danger fw-bolder fs-5'>No projects uploaded yet!!!</p>
+        }
+      </div>
+    </div>
   )
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,31 +9,31 @@ function Profile() {
       <div className="d-flex justify-content-between">
         <h2 className='overflow-hidden'>MY Profile</h2>
         <button onClick={() => setOpen(!open)} className="btn btn-outline-info">
-        <i className="fa-solid fa-chevron-down overflow-hidden"></i>
+          <i className="fa-solid fa-chevron-down overflow-hidden"></i>
         </button>
-                                                                                                                                                                                                                                                                                 
-        </div>
-       <Collapse in={open}>
-          <div className="row justify-content-center mt-2">
-            {/* Upload profile */}
-            <label className="text-center">
-              <input style={{ display: 'none' }} type="file" />
-              <img
-                style={{ height: '200px', width: '200px' }}
-                src="https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg="
-                alt="Profile"
-              />
-            </label>
-            <div className="mt-3">
-              <input type="text" className="form-control mb-2" placeholder="Username" />
-              <input type="text" className="form-control mb-2" placeholder="GitHub" />
-              <input type="text" className="form-control" placeholder="LinkedIn" />
-            </div>
-            <div>
-            <button className='btn btn-success mt-2 w-100'>Update</button>
-            </div>
+
+      </div>
+      <Collapse in={open}>
+        <div className="row justify-content-center mt-2">
+          {/* Upload profile */}
+          <label className="text-center">
+            <input style={{ display: 'none' }} type="file" />
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg="
+              alt="Profile"
+            />
+          </label>
+          <div className="mt-3">
+            <input type="text" className="form-control mb-2" placeholder="Username" />
+            <input type="text" className="form-control mb-2" placeholder="GitHub" />
+            <input type="text" className="form-control" placeholder="LinkedIn" />
           </div>
-       </Collapse>
+          <div>
+            <button className='btn btn-success mt-2 w-100'>Update</button>
+          </div>
+        </div>
+      </Collapse>
 
     </div>
   );
